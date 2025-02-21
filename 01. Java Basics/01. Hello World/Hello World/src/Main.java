20c074925d02ead785a401f1d139e87a942043c4
Gurjot Saini <gurjot.saini129@gmail.com>
2025-02-20 21:12:44 -0500

    This code shows how to print "Hello World" to the standard output

---
+++ 01. Java Basics/01. Hello World/Hello World/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //prints out the Hello World to the Standard Output
+        System.out.println("Hello World!");
+    }
+}
